b56c8348dc66ee6460483b65a2e2df97f11b097b
matt-handy <matt.e.handy@gmail.com>
2023-10-24 15:24:43 -0400

    Add safe checking in email cleanup

--- src/util/test/EmailHelper.java
+++ src/util/test/EmailHelper.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Properties;
 
+import c2.Constants;
 import c2.smtp.EmailHandler;
 import c2.smtp.SimpleEmail;
 
@@ -11,18 +12,21 @@ public class EmailHelper {
 	public static Properties setup() {
 		return ClientServerTest.getDefaultSystemTestProperties();
 	}
-	
+
 	public static void flushC2Emails() {
-		EmailHandler receiveHandler = new EmailHandler();
-		try {
-			receiveHandler.initialize(null, setup(), null, null);
-		} catch (Exception ex) {
-			fail(ex.getMessage());
-		}
-		
-		SimpleEmail email = receiveHandler.getNextMessage();
-		while(email != null) {
-			email = receiveHandler.getNextMessage();
+		Properties prop = setup();
+		if (prop.get(Constants.COMMSERVICES).toString().contains("c2.smtp.EmailHandler")) {
+			EmailHandler receiveHandler = new EmailHandler();
+			try {
+				receiveHandler.initialize(null, prop, null, null);
+			} catch (Exception ex) {
+				fail(ex.getMessage());
+			}
+
+			SimpleEmail email = receiveHandler.getNextMessage();
+			while (email != null) {
+				email = receiveHandler.getNextMessage();
+			}
 		}
 	}
 }
